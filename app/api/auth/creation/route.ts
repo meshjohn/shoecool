1df25321941eea1cafa5962a0e80f2f25c6a3b3f
meshJohn <meshjohn1@gmail.com>
2024-09-17 04:40:29 +0300

    initial commit

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -28,5 +28,9 @@ export async function GET() {
       },
     });
   }
-  return NextResponse.redirect("http://localhost:3000/");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://shoecool.vercel.app/"
+  );
 }
